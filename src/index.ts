f18fb5f5a10236042e3550fc642e5465588411f3
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2021-01-16 00:36:22 +0100

    Fix starting the bot

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,31 @@
-// load env file
 import * as dotenv from 'dotenv'
 import * as fs from 'fs'
+import * as path from 'path'
+import { IMG_PATH, GAMES_PATH, SAVES_PATH } from './constants'
 import bot from './bot'
 
-dotenv.config()
+dotenv.config({
+    path: path.join(__dirname, '.env')
+})
 
 
 const createCacheFolders = () => {
-    process.env.IMG_PATH = process.env.IMG_PATH ?? 'img/'
-    process.env.SAVES_PATH = process.env.SAVES_PATH ?? 'saves/'
-    process.env.GAMES_PATH = process.env.GAMES_PATH ?? 'games/'
-
-    const folders = [process.env.IMGS_PATH!, process.env.SAVES_PATH!, process.env.GAMES_PATH!]
+    const folders = [IMG_PATH, GAMES_PATH, SAVES_PATH]
     folders.forEach((path: string) => {
         if (!fs.existsSync(path)) {
             fs.mkdirSync(path)
         }
     })
 }
-createCacheFolders()
-bot.login(process.env.DISCORD_TOKEN)
+
+const start = async () => {
+    createCacheFolders()
+    try {
+        const token = process.env.DISCORD_TOKEN
+        await bot.login(token)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+start()
