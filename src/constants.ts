75258101a21f1037d521ea2070e67da3017c50b3
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2021-01-15 23:32:20 +0100

    Add constants to seperate file

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+
+export enum ModeEnum {
+    community = 1,
+    hybrid = 2,
+    personal = 3,
+    continuous = 4
+}
+
+// cache paths
+export const IMG_PATH = 'imgs/'
+export const SAVES_PATH = 'saves/'
+export const GAMES_PATH = 'games/'
+
+// canvas
+export const defaultCanvasWidth = 144;
+export const defaultCanvasHeight = 160;
