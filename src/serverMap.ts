d1613a6a1cbb43053fd50e5fc017f684da4e2468
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2021-01-17 02:41:18 +0100

    Add server map

---
+++ src/serverMap.ts
@@ -0,0 +1,29 @@
+import ServerEmulator from './serverEmulator'
+
+
+class ServerMap {
+    private games: Map<number, ServerEmulator>
+
+    constructor() {
+        this.games = new Map<number, ServerEmulator>()
+    }
+
+    public createEmulator(id: number) {
+        const emulator = new ServerEmulator(id)
+        this.games.set(id, emulator)
+        return emulator
+    }
+
+    public getEmulator(id: number) {
+        return this.games.get(id)
+    }
+
+    public destroyEmulator(id: number) {
+        return this.games.delete(id)
+    }
+}
+
+
+// export an instance of the class because we only need one instance of this
+const serverMap = new ServerMap()
+export default serverMap
