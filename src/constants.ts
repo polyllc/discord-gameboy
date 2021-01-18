e3d982bd7c3b232049350bfa65da43fe22070155
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2021-01-18 01:26:27 +0100

    Add default gif length as constant

--- src/constants.ts
+++ src/constants.ts
@@ -13,5 +13,7 @@ export const GAMES_PATH = 'games/'
 // canvas
 export const defaultCanvasWidth = 144;
 export const defaultCanvasHeight = 160;
+export const defaultGIFLength = 3000; // gif length in ms
+
 // TODO make prefix dynamic (changable per guild)
 export const PREFIX = 'gb!'
